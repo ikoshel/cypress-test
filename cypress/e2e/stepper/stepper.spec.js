97fe1288d7d015e58dce242281a9912df13cb383
Ihor Kosheliev <ihor.kosheliev@mev.com>
2023-10-23 02:58:54 +0300

    Refactor stepper test assertions for readability
    The tests in `stepper.spec.js` have been updated to reduce redundancy and improve readability. The `expect()` assertions have been removed, as they were an unnecessary additional check given the preceding `should()` assertions. The visibility assertions for `@stepperTitle` and `@nextButton` have been consolidated into a single line chained with the respective text and disabled checks, making the tests more concise.

--- cypress/e2e/stepper/stepper.spec.js
+++ cypress/e2e/stepper/stepper.spec.js
@@ -7,39 +7,35 @@ describe("Stepper page", () => {
     cy.get('nb-stepper[orientation="vertical"] h3').as("stepperTitle")
     cy.get('nb-stepper[orientation="vertical"] button').last().as("nextButton")
 
-    cy.get("@stepperTitle").should('be.visible').and('have.text', "Step content #1").then(($el) => {
-      expect($el.text()).to.be.eq("Step content #1")
-    })
+    cy.get("@stepperTitle").should('be.visible').and('have.text', "Step content #1")
     cy.get("@nextButton").click()
 
-    cy.get("@stepperTitle").should('be.visible')
-    cy.get("@stepperTitle").should('have.text', "Step content #2")
+    cy.get("@stepperTitle").should('be.visible').should('have.text', "Step content #2")
     cy.get("@nextButton").click()
 
-    cy.get("@stepperTitle").should('have.text', "Step content #3")
+    cy.get("@stepperTitle").should('be.visible').should('have.text', "Step content #3")
     cy.get("@nextButton").click()
 
-    cy.get("@stepperTitle").should('have.text', "Step content #4")
-    cy.get("@nextButton").should('be.disabled')
+    cy.get("@stepperTitle").should('be.visible').should('have.text', "Step content #4")
+
+    cy.get("@nextButton").should('be.visible').should('be.disabled')
   });
 
   it('Horizontal stepper should display correct title on each step', () => {
     cy.get('nb-stepper[orientation="horizontal"] h3').as("stepperTitle")
     cy.get('nb-stepper[orientation="horizontal"] button').last().as("nextButton")
 
-    cy.get("@stepperTitle").should('be.visible').and('have.text', "Step content #1").then(($el) => {
-      expect($el.text()).to.be.eq("Step content #1")
-    })
+    cy.get("@stepperTitle").should('be.visible').and('have.text', "Step content #1")
     cy.get("@nextButton").click()
 
-    cy.get("@stepperTitle").should('be.visible')
-    cy.get("@stepperTitle").should('have.text', "Step content #2")
+    cy.get("@stepperTitle").should('be.visible').should('have.text', "Step content #2")
     cy.get("@nextButton").click()
 
-    cy.get("@stepperTitle").should('have.text', "Step content #3")
+    cy.get("@stepperTitle").should('be.visible').should('have.text', "Step content #3")
     cy.get("@nextButton").click()
 
-    cy.get("@stepperTitle").should('have.text', "Step content #4")
-    cy.get("@nextButton").should('be.disabled')
+    cy.get("@stepperTitle").should('be.visible').should('have.text', "Step content #4")
+
+    cy.get("@nextButton").should('be.visible').should('be.disabled')
   });
 })
